6ed06ff16776952b6154d85bb994db7548a3821a
Chetan <joshic626@gmail.com>
2024-09-15 15:11:24 +0530

    integration wallet connect in app router

---
+++ components/ConnectButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+
+export default function ConnectButton() {
+  const { open } = useWeb3Modal()
+
+  return (
+    <>
+      <button onClick={() => open()}>Connect Wallet</button>
+      <button onClick={() => open({ view: 'Networks' })}>Switch Network</button>
+    </>
+  )
+}
---
+++ config/index.tsx
@@ -0,0 +1,25 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+export const metadata = {
+  name: 'WalletConnect App',
+  description: 'WalletConnect App Example',
+  url: 'https://walletconnect.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+})
