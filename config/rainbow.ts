cf41164e10d324d59ee95fd08a01f3d7c862f751
Chetan <joshic626@gmail.com>
2024-10-24 23:44:55 +0530

    removing rainbow kit from dependencies

--- config/rainbow.ts
+++ config/rainbow.ts
@@ -1,13 +1,12 @@
 // import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base, baseSepolia } from "wagmi/chains";
+// import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+// import { base, baseSepolia } from "wagmi/chains";
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+// export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
-export const config = getDefaultConfig({
-  appName: "Opinion Club",
-  projectId: projectId || "",
-  chains: [baseSepolia, base],
-  ssr: true,
-});
-
+// export const config = getDefaultConfig({
+//   appName: "Opinion Club",
+//   projectId: projectId || "",
+//   chains: [baseSepolia, base],
+//   ssr: true,
+// });
