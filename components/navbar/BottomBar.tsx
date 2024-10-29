5591ace277ed06f230e87271c0b11d47dc175046
Chetan <joshic626@gmail.com>
2024-10-29 15:36:22 +0530

    removing unwanted variables

--- components/navbar/BottomBar.tsx
+++ components/navbar/BottomBar.tsx
@@ -5,7 +5,7 @@ import { useAccount } from "wagmi";
 import { ExploreIcon, HomeIcon, PortfolioIcon } from "./icons/icons";
 
 function BottomBar() {
-  const { address, isConnected } = useAccount();
+  const { address } = useAccount();
   return (
     <footer className="fixed md:hidden  bottom-0 bg-black/10  backdrop-filter bg-opacity-30 backdrop-blur-md  w-screen border-t-[0.5px] right-0 border-gray-400">
       <div className={"py-2 px-3 flex justify-between z-40"}>
